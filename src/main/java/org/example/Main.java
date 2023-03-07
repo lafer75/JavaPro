dd4f7ea00450b2875b969240602418dc7ef0af52
lafer75 <fafo8900@gmail.com>
2023-03-07 20:05:36 +0200

    Homework_24

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,69 @@
+package org.example;
+
+public class Main {
+
+    public static double add(double i, double d) {
+        return i + d;
+    }
+
+    public static double subtract(double i, double d) {
+        return i - d;
+    }
+
+    public static double multiply(double i, double d) {
+        return i * d;
+    }
+
+    public static double divide(double i, double d) {
+        return i / d;
+    }
+
+    public static int dInt(int i, int d) {
+        return i / d;
+    }
+
+    public static int dRem(int i, int d) {
+        return i % d;
+    }
+
+    public static double sq(double i) {
+        return i * i;
+    }
+
+    public static double power(double i, double d) {
+        return Math.pow(i, d);
+    }
+
+    public static double sqrt(double i) {
+        return Math.sqrt(i);
+    }
+
+    public static double abs(double i) {
+        return Math.abs(i);
+    }
+
+    public static double round(double i) {
+        return Math.round(i);
+    }
+
+    public static double log(double i) {
+        return Math.log(i);
+    }
+
+    public static double sin(double i) {
+        return Math.sin(i);
+    }
+
+    public static double cos(double i) {
+        return Math.cos(i);
+    }
+
+    public static double tan(double i) {
+        return Math.tan(i);
+    }
+
+    public static double atan(double i) {
+        return Math.atan(i);
+    }
+
+}
